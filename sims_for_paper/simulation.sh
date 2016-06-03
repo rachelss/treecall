3854799e87c3bef442ad613552dfb20309168860
Rachel Schwartz <rsschwartz@gmail.com>
2016-06-03 15:11:10 -0700

    check for phylip and samtools; use pytho2

--- sims_for_paper/simulation.sh
+++ sims_for_paper/simulation.sh
@@ -1,4 +1,4 @@
-
+#!/usr/bin/env bash
 #bcftools 1.3; Using htslib 1.3
 
 N=$1
@@ -17,6 +17,11 @@ treecall = ../treecall.py
 pyfilter = find_polymorphic_sites.py
 mkphylip = vcf2seq.py
 #phylip tools must be installed and in path dnaml, dnacomp, dnadist, neighbor
+
+which dnaml &>/dev/null
+    [ $? -eq 0 ] || { echo "Phylip must be installed to run. The installation folder must be in your path. Aborting."; exit 1; }
+which samtools &>/dev/null
+    [ $? -eq 0 ] || { echo "Samtools must be installed to run. The installation folder must be in your path. Aborting."; exit 1; }
 
 #commented out b/c using previous sims
 ## make BAMs
@@ -48,29 +53,29 @@ echo "bcfcall done"
 
 # prepare for treecall - output of bcftools view not compatible with vcf.Reader in pyfilter
 bcftools view -v snps -i 'ADF[0]>0 & ADF[1]>0 & ADR[0]>0 & ADR[1]>0' $dir/$dir.bcf | #get just snps; reads on both strands to ensure no weird indels - coudl actually test whether prob or quality is same forward or reverse
-python $pyfilter - 'AD:2;PL:60' | #pipe to filter to check both alleles have more than min reads AND both het and homo present AND likely more than one geno across all samples
+python2 $pyfilter - 'AD:2;PL:60' | #pipe to filter to check both alleles have more than min reads AND both het and homo present AND likely more than one geno across all samples
 bgzip -c > $dir/$dir.candidate1.vcf.gz  #pipe to zip; Skip lines started with character CHAR. [#]
 
 #fix below to allow for gz
-python $pyfilter $dir/$dir.candidate1.vcf.gz 'AD4:1' | bgzip -c > $dir/$dir.candidate2.vcf.gz #filter for at least F or R has 1 for both refs and alts
+python2 $pyfilter $dir/$dir.candidate1.vcf.gz 'AD4:1' | bgzip -c > $dir/$dir.candidate2.vcf.gz #filter for at least F or R has 1 for both refs and alts
 #not sure this is dif from filtering just AD
 
 # infer tree
 #fix below to allow for gz
-#python $treecall part -m 60 -v 60 -e 30 $dir/$dir.candidate2.vcf.gz $dir/$dir.treecall-pt
-python $treecall nbjoin -m 60 -v 60 -e 30 $dir/$dir.candidate2.vcf.gz $dir/$dir.treecall-nj
+#python2 $treecall part -m 60 -v 60 -e 30 $dir/$dir.candidate2.vcf.gz $dir/$dir.treecall-pt
+python2 $treecall nbjoin -m 60 -v 60 -e 30 $dir/$dir.candidate2.vcf.gz $dir/$dir.treecall-nj
 
 # treecall genotype
-bcftools view $dir/$dir.bcf | python $pyfilter - 'AD:2' | python $treecall gtype -t $dir/$dir.treecall-pt.nwk -m 60 -e 30 - $dir/$dir.tc.txt
+bcftools view $dir/$dir.bcf | python2 $pyfilter - 'AD:2' | python2 $treecall gtype -t $dir/$dir.treecall-pt.nwk -m 60 -e 30 - $dir/$dir.tc.txt
 awk '$5>0.5' $dir/$dir.tc.txt > $dir/$dir.tc.p50.txt
 
-bcftools view $dir/$dir.bcf | python $pyfilter - 'AD:2' | python $treecall gtype -t ms.nwk -m 60 -e 30 - $dir/$dir.ms.txt
+bcftools view $dir/$dir.bcf | python2 $pyfilter - 'AD:2' | python2 $treecall gtype -t ms.nwk -m 60 -e 30 - $dir/$dir.ms.txt
 awk '$5>0.5' $dir/$dir.ms.txt > $dir/$dir.ms.p50.txt
 echo "treecall done"
 
 # phylip
 mkdir -p phylip
-bcftools view -v snps $dir/$dir.snp.vcf.gz | python $mkphylip - phylip > phylip/$dir.snp.phylip
+bcftools view -v snps $dir/$dir.snp.vcf.gz | python2 $mkphylip - phylip > phylip/$dir.snp.phylip
 cd phylip
 rm -f outtree outfile
 echo "$dir.snp.phylip" > phylip_inputs.list
