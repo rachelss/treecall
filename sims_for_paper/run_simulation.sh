634e76caf1613e28e05d32fbedc743fa61b5e820
Rachel Schwartz <rsschwartz@gmail.com>
2016-06-03 15:07:26 -0700

    add sim scripts

---
+++ sims_for_paper/run_simulation.sh
@@ -0,0 +1,8 @@
+sh scripts/simulation.sh 10 5 ref/chr22_20-21M.fa
+sh scripts/simulation.sh 10 7 ref/chr22_20-21M.fa
+sh scripts/simulation.sh 10 10 ref/chr22_20-21M.fa
+sh scripts/simulation.sh 10 15 ref/chr22_20-21M.fa
+sh scripts/simulation.sh 10 20 ref/chr22_20-21M.fa
+sh scripts/simulation.sh 10 30 ref/chr22_20-21M.fa
+sh scripts/simulation.sh 10 40 ref/chr22_20-21M.fa
+sh scripts/simulation.sh 10 50 ref/chr22_20-21M.fa
