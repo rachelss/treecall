3a57e2f0ad44744aa7d5a189c7b2a55c64048bf2
Rachel Schwartz <rsschwartz@gmail.com>
2016-09-02 15:17:29 -0400

    add comments and header

--- sims_for_paper/rearrange_ms_dwgsim.py
+++ sims_for_paper/rearrange_ms_dwgsim.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python2
+
 import sys
 import string
 import random
@@ -44,37 +46,39 @@ def get_strands(num_sites):
     strands = [random.randint(1,2) for i in range(num_sites)]
     return strands
 
-###############
-folder = os.path.dirname(sys.argv[1])
 
-msfile = open(sys.argv[1],'r')
-msoutput = msfile.readlines()
-cmd = msoutput[0].split()
-num_samp = int(cmd[1])
-num_sites = int(cmd[4])
 
-pos = get_pos(msoutput[6].split())
-
-handle = open('ref/chr22_20-21M.fa', "rU")
-ref = SeqIO.to_dict(SeqIO.parse(handle, "fasta"))
-handle.close()
-
-refbases = get_refbases(ref['chr22'].seq,pos)
-refbases = [r.upper() for r in refbases]
-altbases = get_altbases(refbases)
-strands = get_strands(num_sites)
-
-for i in range(num_samp):
-    v = str(i+1)
-    dwgsimfile = open(folder+'/var/'+v+'.variants.txt','w')
-    linenum = i-num_samp
-    line = msoutput[linenum]
-    for j in range(num_sites):
-        if int(line[j]) == 1:
-            #chr    #pos    #refbase    #altbase/iupac  #strand
-            dwgsimfile.write("chr22\t"+str(pos[j]+1)+"\t"+refbases[j]+"\t"+altbases[j]+"\t"+str(strands[j])+"\n")
-    dwgsimfile.close()         
-
-treefile = open(sys.argv[1].replace('.output','.nwk'),'w')
-treefile.write(msoutput[4])
-treefile.close()
+if __name__ == "__main__":
+    folder = os.path.dirname(sys.argv[1])
+    
+    msfile = open(sys.argv[1],'r')
+    msoutput = msfile.readlines()
+    cmd = msoutput[0].split()
+    num_samp = int(cmd[1])
+    num_sites = int(cmd[4])
+    
+    pos = get_pos(msoutput[6].split()) #takes list of float-as-str, returns list of int
+    
+    handle = open('ref/chr22_20-21M.fa', "rU")
+    ref = SeqIO.to_dict(SeqIO.parse(handle, "fasta"))
+    handle.close()
+    
+    refbases = get_refbases(ref['chr22'].seq,pos)  #returns list of reference bases for each variant position in pos
+    refbases = [r.upper() for r in refbases]
+    altbases = get_altbases(refbases)  #returns list of hets for each variant position
+    strands = get_strands(num_sites)
+    
+    for i in range(num_samp):
+        v = str(i+1)
+        dwgsimfile = open(folder+'/var/'+v+'.variants.txt','w')
+        linenum = i-num_samp
+        line = msoutput[linenum]
+        for j in range(num_sites):
+            if int(line[j]) == 1:
+                #chr    #pos    #refbase    #altbase/iupac  #strand
+                dwgsimfile.write("chr22\t"+str(pos[j]+1)+"\t"+refbases[j]+"\t"+altbases[j]+"\t"+str(strands[j])+"\n")
+        dwgsimfile.close()         
+    
+    treefile = open(sys.argv[1].replace('.output','.nwk'),'w')
+    treefile.write(msoutput[4])
+    treefile.close()
